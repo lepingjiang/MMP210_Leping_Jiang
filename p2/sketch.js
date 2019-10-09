20bd63d4ad6fa086db4cf4407a47436a8d54efea
lepingjiang <54827306+lepingjiang@users.noreply.github.com>
2019-10-09 17:40:27 -0400

    project 2 added

--- p2/sketch.js
+++ p2/sketch.js
@@ -4,10 +4,16 @@
 	10/3/19
 */
 
-var oa
+var fanCase
+var x = 10//X location
+var y = 50//Y location
+var imgWidth = 640//imgage wdith
+var imgHeight = 360//image height
+var textWidth = 600//text width
+
 
 function preload(){
-	oa = loadImage(oa.jpg);
+	fanCase = loadImage("6089b779763a5ed0aea1ddb91977856f.jpg");
 }
 
 
@@ -20,24 +26,30 @@ function setup() {
 function draw() {
 	background("white");
 	
-	image(oa, 0, 0);
+	image(fanCase, x-10, y-50, imgWidth, imgHeight);
 	
 	textAlign(LEFT, BOTTOM);
-	strokeWeight(0);
-	textSize(20);
-	text("MMP210", 100, 100);
+	strokeWeight(3);
+	textSize(30);
+	text("FirstTime builder ask:", x, y, textWidth);
 	
 	
-	stroke("lightblue");
+	stroke("#ffaaaa");
 	strokeWeight(5);
 	textStyle(ITALIC);
-	textSize(100);
-	text("hello world", 100, 200);
+	textSize(30);
+	text("How many fan am I going to need for my PC?", x, y+40, textWidth);
 	
+	strokeWeight(3);
+	textSize(30);
+	text("Expert answer:", x, y+240, textWidth);
 	
-	textSize(10);
-	text("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse earum quod exercitationem vero, temporibus veritatis in ea voluptates dolore doloremque amet sint, doloribus blanditiis maiores enim qui fuga. Adipisci, quaerat?", 100, 250, 500);
+	strokeWeight(5);
+	textSize(30);
+	text("You need a case that is made out of fans.", x, y+280, textWidth);
 	
-	
+	/*triangle(320, 180, 150, 180, 170, 230);
 
+	
+	triangle(320, 180, 150, 180, 170, 230);*/
 }
