6ec3cb2780d9983d3b9ec0431ce776a51dbd2c4d
lepingjiang <54827306+lepingjiang@users.noreply.github.com>
2019-12-10 15:13:41 -0500

    final

--- final/sketch.js
+++ final/sketch.js
@@ -3,6 +3,7 @@
 */
 
 var serial;
+var serialAvailable;
 var portName = "COM8";
 var sensorValue = 51;
 var candle;//candle imgae
@@ -13,8 +14,14 @@ var ly = 140;//location y
 var iw = 60;//image width
 var ih = 150;//image hight
 var ib = 255;//image brightness
-var iop = 50;//image opacity
-var iops = 5;//image opacity change
+var iop = 170;//image opacity
+var iops = 2;//image opacity change
+/*var iop2 = 120;//image opacity 2
+var iops2 = 2;//image opacity change 2
+var iop3= 80;//image opacity 2
+var iops3 = 1.5;//image opacity change 2*/
+
+
 
 function preload() {
 	candle = loadImage("asset1.png");
@@ -41,15 +48,18 @@ function serverConnected() {
 }
 
 function portOpen() {
-	console.log('the serial port opened.')
+	console.log('the serial port opened.');
+	serialAvailable = true;
 }
 
 function portClose() {
 	console.log('The serial port closed.');
+	serialAvailable = false;
 }
 
 function serialError() {
 	console.log("error");
+	serialAvailable = false;
 }
 
 function serialEvent() {
@@ -77,21 +87,58 @@ function draw(){
 	if (keyIsPressed && mouseIsPressed){
 		tint(ib, iop);
 		image(firea, lx+90, ly-120, iw, ih);
+		//tint(ib, iop2);
+		//image(fireb, lx+85, ly-80, iw*0.2, ih*0.2);
+		//image(fireb, lx+140, ly-120, iw*0.2, ih*0.2);
+		//flashing(lx+85, ly-80, iw*0.2, ih*0.2);
 	}
 	
 	iop += iops;
-	
-	if (iop > 200 || iop < 0) {
+	if (iop > 255 || iop < 170) {
 		iops *= -1;
 	}
-	
 
+/*	iop2 += iops2;
+	if (iop2 > 240 || iop2 < 120) {
+		iops2 *= -1;
+	}
+
+	iop3 += iops;
+	if (iop > 255 || iop < 170) {
+		iops *= -1;
+	}*/
+	
+	
+	var iop2 = 100;//image base opacity
+	var iops2 = 1;//image opacity changer
+	
+	tint(ib, iop2);
+	image(fireb, lx+140, ly-120, iw*0.2, ih*0.2);
+	
+	iop2 += iops2;
+	
+	if (iop2 > 255 || iop2 < 100) {
+		iops2 *= -1;
+	}
 }
 
 function mousePressed(){
 
 }
 
+/*function flashing(x, y, w, h, a){
+	var iop2 = 100;//image base opacity
+	var iops2 = 1;//image opacity changer
+	
+	tint(ib, iop2);
+	image(fireb, x, y, w, h);
+	
+	iop2 += iops2;
+	
+	if (iop2 > 255 || iop2 < 100) {
+		iops2 *= -1;
+	}
+}*/
 
 
 
